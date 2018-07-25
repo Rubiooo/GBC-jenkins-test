fe9f68d07219239578c4228cf092f0cdd90fb6fd
Jimmy <jimmy@georgebrown.ca>
2018-07-25 13:59:50 -0400

    change corn schedule

--- common/GlobalVars.groovy
+++ common/GlobalVars.groovy
@@ -3,9 +3,9 @@ package common
 class GlobalVars {
   static def gitBaseUrl= "ssh://git@gitrepo.georgebrown.ca:2222"
 
-  static def scmPollScheduleMin = '* * * * *'
-  static def scmPollScheduleFiveMins = 'H/5 * * * *'
-  static def scmPollScheduleHalfHour = 'H/30 * * * *'
-  static def scmPollScheduleEveryDay = 'H H(3-8) * * *'
-  static def scmPollScheduleSundayMorning = 'H H(4-8) * * 7'
+  static def scheduleMin = '* * * * *'
+  static def scheduleFiveMins = 'H/5 * * * *'
+  static def scheduleHalfHour = 'H/30 * * * *'
+  static def scheduleEveryDay = 'H H(3-8) * * *'
+  static def scheduleSundayMorning = 'H H(4-8) * * 7'
 }
