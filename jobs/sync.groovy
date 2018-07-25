288e24750e594dc619eb5d168ed912d0aa3664c0
Jimmy <jimmy@georgebrown.ca>
2018-07-25 14:58:13 -0400

    sync-repo edit

--- jobs/sync.groovy
+++ jobs/sync.groovy
@@ -2,16 +2,25 @@ import common.GlobalVars
 
 folder('repo-sync')
 
-job('repo-sync/test-1') {
-  steps {
-    shell('''
-    rm -rf banner_student_faculty_grade_entry_app
-    git clone ssh://git@gitrepo.georgebrown.ca:2222/ellucian/banner_student_faculty_grade_entry_app.git
-    cd banner_student_faculty_grade_entry_app
-    git remote add upstream ssh://git@source.ellucian.com/banner/apps/banner_student_faculty_grade_entry_app.git
-    git fetch upstream
-    git merge upstream/master
-    git push origin master
-    ''')
+BuildFramework.sync(this, 'banner/apps', 'application_navigator_app')
+
+class BuildFramework {
+  static sync(dslFactory, repofoler, reponame) {
+    dslFactory.job("repo-sync/${reponame}") {
+      triggers {
+        cron(GlobalVars.scheduleEveryDay)
+      }
+      steps {
+        shell(""" echo ${reponame}
+          rm -rf ${reponame}
+          git clone ssh://git@gitrepo.georgebrown.ca:2222/ellucian/${reponame}.git
+          cd ${reponame}
+          git remote add upstream ssh://git@source.ellucian.com/${repofolder}/${reponame}.git
+          git fetch upstream
+          git merge upstream/master
+          git push origin master
+        """)
+      }
+    }
   }
 }
