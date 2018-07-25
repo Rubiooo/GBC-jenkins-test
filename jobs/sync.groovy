72999b74e1505321eb1789bab4d5fb212844576b
Jimmy <jimmy@georgebrown.ca>
2018-07-25 13:57:52 -0400

    changes on shell and globalvars

--- jobs/sync.groovy
+++ jobs/sync.groovy
@@ -4,7 +4,7 @@ folder('repo-sync')
 
 job('repo-sync/test-1') {
   steps {
-    shell('
+    shell('''
     rm -rf banner_student_faculty_grade_entry_app
     git clone ssh://git@gitrepo.georgebrown.ca:2222/ellucian/banner_student_faculty_grade_entry_app.git
     cd banner_student_faculty_grade_entry_app
@@ -12,6 +12,6 @@ job('repo-sync/test-1') {
     git fetch upstream
     git merge upstream/master
     git push origin master
-    ')
+    ''')
   }
 }
