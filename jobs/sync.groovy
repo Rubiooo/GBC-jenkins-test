f4ab4bc839463d34f8e7a2f5387e1e2416bcf1b0
Jimmy <jimmy@georgebrown.ca>
2018-07-25 13:46:58 -0400

    add globalvars and syncjob

--- jobs/sync.groovy
+++ jobs/sync.groovy
@@ -1,1 +1,17 @@
 import common.GlobalVars
+
+folder('repo-sync')
+
+job('repo-sync/test-1') {
+  steps {
+    shell('
+    rm -rf banner_student_faculty_grade_entry_app
+    git clone ssh://git@gitrepo.georgebrown.ca:2222/ellucian/banner_student_faculty_grade_entry_app.git
+    cd banner_student_faculty_grade_entry_app
+    git remote add upstream ssh://git@source.ellucian.com/banner/apps/banner_student_faculty_grade_entry_app.git
+    git fetch upstream
+    git merge upstream/master
+    git push origin master
+    ')
+  }
+}
