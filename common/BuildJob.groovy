ba933156d2435f20f01e9f170a9334999809246a
Jimmy <jimmy@georgebrown.ca>
2018-07-25 23:46:59 -0400

    create BuildJob.groovy

---
+++ common/BuildJob.groovy
@@ -0,0 +1,25 @@
+package common
+
+class BuildJob {
+  static sync(dslFactory, repofolder, reponame) {
+    def foldername=repofolder.replaceAll('/','_')
+    dslFactory.folder("repo-sync/${foldername}")
+    dslFactory.job("repo-sync/${foldername}/${reponame}") {
+      triggers {
+        cron(GlobalVars.scheduleEveryDay)
+      }
+      steps {
+        shell(""" echo ${reponame}
+rm -rf ${reponame}
+git clone ssh://git@gitrepo.georgebrown.ca:2222/ellucian_${foldername}/${reponame}.git
+cd ${reponame}
+git remote add upstream ssh://git@source.ellucian.com/${repofolder}/${reponame}.git
+git fetch upstream
+git merge upstream/master
+git push origin master
+""")
+      }
+    }
+
+  }
+}
