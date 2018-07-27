2633adaea838a2c1b3f44ea6927ad2829f28d9d8
Jimmy <jimmy@georgebrown.ca>
2018-07-27 15:14:37 -0400

    simplify db.groovy and db-migrate

--- jobs/db.groovy
+++ jobs/db.groovy
@@ -1,12 +1,31 @@
 folder('db')
 
-pipelineJob('db/poc-migrate') {
-  definition {
-    cpsScm {
-      scm {
-        git('ssh://git@gitrepo.georgebrown.ca:2222/devops/jenkins-dsl.git', 'master')
-      }
-      scriptPath("pipelines/dev_migrate.groovy")
+job("db/poc-migrate") {
+    scm {
+        git {
+            remote {
+                name('origin')
+                url('ssh://git@gitrepo.georgebrown.ca:2222/ellucian/gbc/db-migration.git')
+            }
+            branch('master')
+        }
     }
-  }
+
+
+    parameters {
+        choiceParam('env',["dev","qa","prod"])
+        booleanParam('dryrun', true)
+    }
+
+    steps {
+        shell('''
+            docker run -t --rm -e FLYWAY_CONFIG_FILES=${env}.conf -v ${WORKSPACE}/sql:/flyway/sql -v ${WORKSPACE}/conf:/flyway/conf ${flyway} info
+            if [[ $dryrun == false ]]; then
+                docker run -t --rm -e FLYWAY_CONFIG_FILES=${env}.conf -v ${WORKSPACE}/sql:/flyway/sql -v ${WORKSPACE}/conf:/flyway/conf ${flyway} migrate
+            fi
+
+        ''')
+
+    }
+
 }
