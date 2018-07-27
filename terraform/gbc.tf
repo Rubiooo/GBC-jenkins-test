6596ab1db59cb24752db53e5156fcb405f6d2385
Jimmy <jimmy@georgebrown.ca>
2018-07-27 14:27:33 -0400

    add gbc project repo

---
+++ terraform/gbc.tf
@@ -0,0 +1,21 @@
+resource "gitlab_group" "gbc" {
+  name             = "GBC customization"
+  path             = "gbc"
+  visibility_level = "internal"
+  description      = "The source repo from GBC customization"
+  parent_id        = "${gitlab_group.ellucian.id}"
+}
+
+resource "gitlab_project" "db-migration" {
+  name             = "db-migration"
+  description      = "The source repo from GBC DB migration"
+  namespace_id     = "${gitlab_group.db-migration.id}"
+  issues_enabled   = false
+  wiki_enabled     = false
+  snippets_enabled = false
+  visibility_level = "internal"
+
+  lifecycle {
+    ignore_changes = "default_branch"
+  }
+}
