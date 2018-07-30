34ccf4534861f08c3ff6b55b34726717eb72c193
Jimmy <jimmy@georgebrown.ca>
2018-07-30 15:03:52 -0400

    fix infrastructure 01

--- jobs/infrastructure.groovy
+++ jobs/infrastructure.groovy
@@ -4,7 +4,7 @@ pipelineJob('infrastructure/bitbucket') {
     definition {
         cpsScm {
             scm {
-                git('ssh://git@gitrepo.georgebrown.ca:7999/devops/jenkins-dsl.git', 'master')
+                git('ssh://git@gitrepo.georgebrown.ca:7999/dev/jenkins-dsl.git', 'master')
             }
             scriptPath("pipelines/bitbucket.groovy")
         }
