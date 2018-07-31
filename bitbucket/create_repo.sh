451000f33e9ad1ef5bd97c0ed2389842e8982dee
Jimmy <jimmy@georgebrown.ca>
2018-07-31 12:30:09 -0400

    changes on create_repo.sh and delete bitbucket.groovy

--- bitbucket/create_repo.sh
+++ bitbucket/create_repo.sh
@@ -5,9 +5,9 @@ password=XXXX
 api=https://gitrepo.georgebrown.ca/rest/api/1.0
 
 while read -r line; do
-  if [ "$(echo ${line} |egrep -c "^#")" -eq 1]; then continue; fi
+  if [ "$(echo ${line} | grep -E -c "^#")" -eq 1 ] continue; fi
   IFS=', ' read project repo <<< "${line}"
   echo -e "create ${repo} in ${project}"
-  curl -k -X POST -u ${user}:${password} -H "Content-Type: application/json" \
-    ${api}/projects/${project}/repos  -d "{\"name\": \"${repo}\",\"scmId\": \"git\" }"
+  curl -k -X POST -u ${username}:${password} -H "content-type: application\jason" \
+  ${api}/projects/${project}/repos -d "{ \"name\": \"${repo}\", \"scmId\": \"git\" }"
 done < "repolist.txt"
