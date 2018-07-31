40d62a125e3ca58705b127c76f5553e7f70f4f98
Jimmy <jimmy@georgebrown.ca>
2018-07-31 17:11:09 -0400

    update sync job -02

--- common/BuildJob.groovy
+++ common/BuildJob.groovy
@@ -12,6 +12,7 @@ class BuildJob {
         shell(""" echo ${reponame}
 rm -rf ${reponame}
 git clone -b master ssh://git@source.ellucian.com/${repofolder}/${reponame}.git
+cd ${reponame}
 git remote add local ssh://git@gitrepo.georgebrown.ca:7999/el/${foldername}_${reponame}.git
 git push local master --force
 """)
