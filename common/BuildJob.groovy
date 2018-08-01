82fa2d30056ef8bed2753320f7e07c6c6b91ddcc
Jimmy <jimmy@georgebrown.ca>
2018-07-31 23:19:58 -0400

    push all tages to remote

--- common/BuildJob.groovy
+++ common/BuildJob.groovy
@@ -14,7 +14,7 @@ rm -rf ${reponame}
 git clone -b master ssh://git@source.ellucian.com/${repofolder}/${reponame}.git
 cd ${reponame}
 git remote add local ssh://git@gitrepo.georgebrown.ca:7999/el/${foldername}_${reponame}.git
-git push local master --force
+git push local master --force --tags
 """)
       }
     }
