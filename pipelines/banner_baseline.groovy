0454de99df0f5fa033b1b9e61a8bf33efec210de
Jimmy <jimmy@georgebrown.ca>
2018-08-01 16:24:18 -0400

    add maven build

--- pipelines/banner_baseline.groovy
+++ pipelines/banner_baseline.groovy
@@ -5,12 +5,22 @@ node {
      checkout([
         $class: 'GitSCM'
         branches: [[name: "master"]]
-        userRemoteConfigs: [[url: 'ssh://git@gitrepo.georgebrown.ca:7999/gbc/banner_pages.git']]        
+        userRemoteConfigs: [[url: 'ssh://git@gitrepo.georgebrown.ca:7999/gbc/banner_pages.git']]
      ])
         }
 
         stage('update pom') {
-            sh "ls -l"
+            sh "curl 'https://gitrepo.georgebrown.ca/projects/GBC/repos/banner_pages_fix/raw/base/net.hedtech.banner/pom.xml?at=refs%2Fheads%2Fmaster' -o base/net.hedtech.banner/pom.xml"
+            sh "curl 'https://gitrepo.georgebrown.ca/projects/GBC/repos/banner_pages_fix/raw/build/net.hedtech.banner.hr/pom.xml?at=refs%2Fheads%2Fmaster' -o build/net.hedtech.banner.hr/pom.xml"
+            sh "curl 'https://gitrepo.georgebrown.ca/projects/GBC/repos/banner_pages_fix/raw/reports/pom.xml?at=refs%2Fheads%2Fmaster' -o reports/pom.xml"
+        }
+
+        stage('maven build') {
+            withCredentials([file(credentialsId: 'mvnsettings', variable: 'MVNSETTINGS')]) {
+                try {
+                    sh "docker run -t --rm -v `pwd`:/app maven:3 -v ${MVNSETTINGS}:/root/.m2/settings.xml mvn -f /app/build/net.hedtech.banner.hr/pom.xml compile"
+                } catch (e) {}
+            }
         }
     }
 }
