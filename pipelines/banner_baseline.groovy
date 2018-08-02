bef625709803322a69d046a54552e16bb0e65201
Jimmy <jimmy@georgebrown.ca>
2018-08-02 12:35:14 -0400

    no docker build

--- pipelines/banner_baseline.groovy
+++ pipelines/banner_baseline.groovy
@@ -7,13 +7,13 @@ node {
             branches: [[name: "master"]]
             userRemoteConfigs: [[url: 'ssh://git@gitrepo.georgebrown.ca:7999/gbc/banner_pages.git']]
          ])
-           dir ("_fix") {
-               checkout([
-                 $class: 'GitSCM',
-                 branches: [[name: "master"]],
-                 userRemoteConfigs: [[url: 'ssh://git@gitrepo.georgebrown.ca:7999/gbc/banner_pages_fix.git']]
-                 ])
-           }
+          // dir ("_fix") {
+          //     checkout([
+          //       $class: 'GitSCM',
+          //       branches: [[name: "master"]],
+          //       userRemoteConfigs: [[url: 'ssh://git@gitrepo.georgebrown.ca:7999/gbc/banner_pages_fix.git']]
+          //       ])
+          // }
 
            dir ("_gbc") {
                 checkout([
@@ -25,7 +25,7 @@ node {
         }
 
         stage('fix_repo') {
-          sh ("yes|cp -rf _fix/* .")
+          // sh ("yes|cp -rf _fix/* .")
           sh ("yes|cp -rf _gbc/* .")
         }
 
@@ -33,18 +33,18 @@ node {
         stage('maven build') {
             withCredentials([file(credentialsId: 'mvnsettings', variable: 'MVNSETTINGS')]) {
                 try {
-                    sh "docker run -t --rm -v `pwd`:/app -v ${MVNSETTINGS}:/root/.m2/settings.xml -v /tmp/m2.repository/:/root/.m2/repository/ maven:3 mvn -f /app/build/net.hedtech.banner.hr/pom.xml deploy"
+                    sh "docker run -t --rm -v `pwd`:/app -v ${MVNSETTINGS}:/root/.m2/settings.xml -v /tmp/m2.repository/:/root/.m2/repository/ maven:3 mvn -f /app/build/net.hedtech.banner.hr/pom.xml package"
                     sh "$curl_login -T ./workspace/webapp-workspace/target/wrksp.war -O \"https://artifactory.georgebrown.ca/artifactory/generic-local/build-${BUILD_NUMBER}/wrksp.war\""
                     sh "$curl_login -T ./build/webapp-services/target/wrksp.ws.war -O \"https://artifactory.georgebrown.ca/artifactory/generic-local/build-${BUILD_NUMBER}/wrksp.ws.war\""
-                } catch (e) {}
+                } catch (e)
             }
         }
 
-        stage('docker build') {
+        // stage('docker build') {
 
-            sh "docker build -t gbc/banner:${BUILD_ID} . --label 'git_commit="+scm.GIT_COMMIT+"'"
-            sh "docker push gbc/banner:${BUILD_ID}"
-            sh "docker rmi gbc/banner:${BUILD_ID}"
-        }
+        //    sh "docker build -t gbc/banner:${BUILD_ID} . --label 'git_commit="+scm.GIT_COMMIT+"'"
+        //    sh "docker push gbc/banner:${BUILD_ID}"
+        //    sh "docker rmi gbc/banner:${BUILD_ID}"
+        //}
     }
 }
