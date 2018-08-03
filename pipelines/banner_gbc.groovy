e37094471192e09d369c72b88bc485cdcc0ce771
Jimmy <jimmy@georgebrown.ca>
2018-08-02 23:34:20 -0400

    add build function to banner_gbc.groovy

--- pipelines/banner_gbc.groovy
+++ pipelines/banner_gbc.groovy
@@ -34,13 +34,21 @@ node {
             withCredentials([file(credentialsId: 'mvnsettings', variable: 'MVNSETTINGS')]) {
                 try {
                     sh "docker run -t --rm -v `pwd`:/app -v ${MVNSETTINGS}:/root/.m2/settings.xml -v /tmp/m2.repository/:/root/.m2/repository/ maven:3 mvn -B -f /app/build/net.hedtech.banner.hr/pom.xml clean package"
-                    sh "$curl_login -T ./workspace/webapp-workspace/target/wrksp.war -O \"https://artifactory.georgebrown.ca/artifactory/generic-local/build-${BUILD_ID}/wrksp.war\""
-                    sh "$curl_login -T ./build/webapp-services/target/wrksp.ws.war -O \"https://artifactory.georgebrown.ca/artifactory/generic-local/build-${BUILD_ID}/wrksp.ws.war\""
-
-                    sh "$curl_login -T ./workspace/webapp-workspace/target/wrksp.war -O \"https://artifactory.georgebrown.ca/artifactory/generic-local/build-gbc/wrksp.war\""
-                    sh "$curl_login -T ./build/webapp-services/target/wrksp.ws.war -O \"https://artifactory.georgebrown.ca/artifactory/generic-local/build-gbc/wrksp.ws.war\""
                 } catch (e)
+                uploadBuild("./workspace/webapp-workspace/target", "wrksp.war")
+                uploadBuild("./build/webapp-services/target", "wrksp.ws.war")
             }
+        }
+
+        def uploadBuild(String path, String filename) {
+          def repo = "https://artifactory.georgebrown.ca/artifactory/generic-local"
+          def now = new Date()
+          def timestamp = now.format("yyyyMMdd-HHmm")
+          def command = "${curl_login} -T ${path}/${filename} -O \"${repo}/build-gbc/${filename}-${timestamp}\""
+          def result =  sh(returnStdout: true, script: command)
+          command = "${curl_login} -T ${path}/${filename} -O \"${repo}/build-gbc-latest/${filename}\""
+          result =  sh(returnStdout: true, script: command)
+          return result
         }
 
         // stage('docker build') {
