4d6fcd7635e6437b08e3b4924cb0833ed438a150
Jimmy <jimmy@georgebrown.ca>
2018-08-02 22:59:15 -0400

    new db job

--- jobs/db.groovy
+++ jobs/db.groovy
@@ -1,40 +1,12 @@
 folder('db')
 
-job("db/poc-migrate") {
-    scm {
-        git {
-            remote {
-                name('origin')
-                url('ssh://git@gitrepo.georgebrown.ca:2222/ellucian/gbc/db-migration.git')
+pipelineJob('db/load_script') {
+    definition {
+        cpsScm {
+            scm {
+                git('ssh://git@gitrepo.georgebrown.ca:7999/gbc/gbcbanner.git', 'master')
             }
-            branch('master')
+            scriptPath(pipelines/build.groovy)
         }
     }
-
-
-    parameters {
-        choiceParam('env',["dev","qa","prod"], "environment name?")
-        booleanParam('dryrun', true, "dry run?")
-        stringParam('target', '', 'FLYWAY_TARGET version')
-    }
-
-    steps {
-        shell('''
-export FLYWAYIMAGE=boxfuse/flyway:5-alpine
-
-if [[ "$target" != "" ]]; then
-    export FLYWAYTARGET="-e FLYWAY_TARGET=$target"
-fi
-
-if [[ $dryrun == true ]]; then
-    docker run -t --rm ${FLYWAYTARGET} -e FLYWAY_CONFIG_FILES=conf/${env}.conf -v ${WORKSPACE}/sql:/flyway/sql -v ${WORKSPACE}/conf:/flyway/conf ${FLYWAYIMAGE} info
-else
-docker run -t --rm ${FLYWAYTARGET} -e FLYWAY_CONFIG_FILES=conf/${env}.conf -v ${WORKSPACE}/sql:/flyway/sql -v ${WORKSPACE}/conf:/flyway/conf ${FLYWAYIMAGE} info
-docker run -t --rm ${FLYWAYTARGET} -e FLYWAY_CONFIG_FILES=conf/${env}.conf -v ${WORKSPACE}/sql:/flyway/sql -v ${WORKSPACE}/conf:/flyway/conf ${FLYWAYIMAGE} migrate
-docker run -t --rm ${FLYWAYTARGET} -e FLYWAY_CONFIG_FILES=conf/${env}.conf -v ${WORKSPACE}/sql:/flyway/sql -v ${WORKSPACE}/conf:/flyway/conf ${FLYWAYIMAGE} info
-fi
-        ''')
-
-    }
-
 }
