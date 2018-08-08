60cc7c81e28c0e61ff9f45b7b3287a8941e7f391
Jimmy <jimmy@georgebrown.ca>
2018-08-08 16:22:14 -0400

    deploy_dev.groovy

--- pipelines/deploy_dev.groovy
+++ pipelines/deploy_dev.groovy
@@ -23,6 +23,7 @@ node {
       sh "$curl_login -sSLO https://artifactory.georgebrown.ca/artifactory/generic-local/build-gbc/latest/wrksp.ws.war"
       sh "ssh $host \"rm -rf /u01/app/tomcat/webapps/wrksp\""
       sh "ssh $host \"rm -rf /u01/app/tomcat/webapps/wrksp.ws\""
+      sh "ssh $host \"rm -rf /u01/app/tomcat/temp\""
       sh "scp wrksp.war $host:/u01/app/tomcat/webapps"
       sh "scp wrksp.ws.war $host:/u01/app/tomcat/webapps"
       sh "ssh $host \"chown tomcat:tomcat /u01/app/tomcat/webapps/*.war\""
