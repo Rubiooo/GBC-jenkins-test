a507debcce235b7d806cf51abea9d1aad371cf51
Jimmy Chen <maplerubiooo@gmail.com>
2018-08-08 23:29:38 -0400

    add new job

--- jobs/db.groovy
+++ jobs/db.groovy
@@ -10,3 +10,14 @@ pipelineJob('db/load_script') {
         }
     }
 }
+
+pipelineJob('db/promote_scripts') {
+    definition {
+        cpsScm {
+            scm {
+                git('ssh://git@gitrepo.georgebrown.ca:7999/gbc/gbcbanner.git', 'master')
+            }
+            scriptPath("promote.groovy")
+        }
+    }
+}
