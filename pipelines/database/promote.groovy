96386b35bf5b83af04049addcd98a6afcbd2c2d7
Jimmy <jimmy@georgebrown.ca>
2018-08-10 15:24:16 -0400

    promote.groovy

--- pipelines/database/promote.groovy
+++ pipelines/database/promote.groovy
@@ -38,6 +38,7 @@ node {
         sh "git checkout -B ${sourceBranch}"
         sh "cp ${sourceEnv}/deployment/install.txt ${targetEnv}/deployment/"
         sh "git add ${targetEnv}/deployment/install.txt"
+        print (userInput)
         for (line in install) {
           if (userInput[line]) {
             sh "cp ${sourceEnv}/deployment/${line}.csv ${targetEnv}/deployment/"
