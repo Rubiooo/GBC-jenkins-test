77b304705db4b9a3552670aac113cc0ed150d10a
Jimmy Chen <maplerubiooo@gmail.com>
2018-08-13 12:51:02 -0400

    add devl configurations

--- pipelines/banner_gbc.groovy
+++ pipelines/banner_gbc.groovy
@@ -50,13 +50,15 @@
                   withCredentials([file(credentialsId: 'mvnsettings', variable: 'MVNSETTINGS')]) {
                     sh "docker run -t --rm -v `pwd`:/app -v ${MVNSETTINGS}:/root/.m2/settings.xml -v /tmp/m2.repository/:/root/.m2/repository/ maven:3 mvn -B -f /app/build/net.hedtech.banner.hr/pom.xml clean package"
                   }
+                  sh "mkdir -p dist"
+                  sh "tar czfv dist/devl.tar.gz -C devl ."
                 } catch (Exception e) {
                     slackSend (channel: 'jenkins', message: ":bangbang: @channel Build failed, please check ${BUILD_URL}")
                     currentBuild.result = 'FAILURE'
                     error('Build failure')
                 }
 
-
+                uploadBuild("./dist", "devl.tar.gz",timestamp)
                 uploadBuild("./workspace/webapp-workspace/target", "wrksp.war", timestamp)
                 uploadBuild("./build/webapp-services/target", "wrksp.ws.war", timestamp)
 
