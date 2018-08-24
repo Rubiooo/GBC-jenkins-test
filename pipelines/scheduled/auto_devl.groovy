5782aeb00b074b16767c31deafec8d143c718ce7
Jimmy Chen <maplerubiooo@gmail.com>
2018-08-24 12:58:13 -0400

    format of auto_devl

--- pipelines/scheduled/auto_devl.groovy
+++ pipelines/scheduled/auto_devl.groovy
@@ -1,23 +1,21 @@
 node {
 
-timestamps {
+  timestamps {
+      slackSend (channel: 'db-promotion', message: "====== auto devl Begin ======")
+      stage("load load_script") {
 
+          build job: '/scripts_promotion/load_script',
+              parameters: [string(name: 'Environment', value: 'devl'),
+                  string(name: 'Username', value: 'jenkins'),
+                  [$class: 'com.michelin.cio.hudson.plugins.passwordparam.PasswordParameterValue', description: 'db password', name: 'Password', value: 'abc123'],
+                  booleanParam(name: 'DryRun', value: true)]
+      }
 
-    stage("Start") {
-        slackSend (channel: 'db-promotion', message: "====== auto devl Begin ======")
+      stage("build and deploy devl") {
+          build job: 'banner_page_build/build_gbc'
 
-        build job: '/scripts_promotion/load_script',
-            parameters: [string(name: 'Environment', value: 'devl'),
-                string(name: 'Username', value: 'jenkins'),
-                [$class: 'com.michelin.cio.hudson.plugins.passwordparam.PasswordParameterValue', description: 'db password', name: 'Password', value: 'abc123'],
-                booleanParam(name: 'DryRun', value: true)]
-
-        build job: 'banner_page_build/build_gbc'
-
-        build job: '/banner_page_build/deploy_dev'
-
-        slackSend (channel: 'db-promotion', message: "====== auto devl Finish ======")
-
-    }
-} //timestamps
+          build job: '/banner_page_build/deploy_dev'
+      }
+      slackSend (channel: 'db-promotion', message: "====== auto devl Finish ======")
+  } //timestamps
 } //node
