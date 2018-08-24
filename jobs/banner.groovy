60e0319683f74a723d5de10334d9ad0d496c3a8a
Jimmy Chen <maplerubiooo@gmail.com>
2018-08-24 13:51:17 -0400

    format

--- jobs/banner.groovy
+++ jobs/banner.groovy
@@ -2,10 +2,10 @@ folder('banner_page_build')
 
 pipelineJob('banner_page_build/build_baseline') {
   description('''Build banner_baseline
-   - purpose: build war files of banner_baseline and upload to artifactory
-   step 1: checkout banner_pages to current root
-   step 2: checkout banner_pages_fix and copy its files to overwrite current root
-   step 3: build war files and upload to artifactory''')
+- purpose: build war files of banner_baseline and upload to artifactory
+step 1: checkout banner_pages to current root
+step 2: checkout banner_pages_fix and copy its files to overwrite current root
+step 3: build war files and upload to artifactory''')
     definition {
         cpsScm {
             scm {
@@ -17,17 +17,13 @@ pipelineJob('banner_page_build/build_baseline') {
 }
 
 pipelineJob('banner_page_build/build_gbc') {
-    description('''Build banner_gbc scheduled to run everyday afternoon at 4:30pm.
-       - purpose: build jar file of banner_gbc and upload to artifcatory
-       step 1: checkout banner_pages to current root
-       step 2: checkout banner_pages_gbc and copy its files to overwrite current root
-       step 3: unzip devl/wrksp.ws and copy to dist/devl.wrksp.ws.tar.gz
-       step 4: build jar files and upload to artifactory
-       step 5: scheduled to run at 4:30pm everyday
+    description('''Build banner_gbc scheduled
+- purpose: build jar file of banner_gbc and upload to artifcatory
+step 1: checkout banner_pages to current root
+step 2: checkout banner_pages_gbc and copy its files to overwrite current root
+step 3: unzip devl/wrksp.ws and copy to dist/devl.wrksp.ws.tar.gz
+step 4: build jar files and upload to artifactory
 ''')
-    triggers {
-        cron('30 16 * * *')
-    }
     definition {
         cpsScm {
             scm {
@@ -40,11 +36,11 @@ pipelineJob('banner_page_build/build_gbc') {
 
 pipelineJob('banner_page_build/deploy_dev') {
     description('''Deploy latest war files to tomcat in DEVL
-    - step 1: stop tomcat
-    - step 2: unzip devl.wrksp.ws.tar.gz and update wrksp.ws.war WEB-INF
-    - step 3: clean tomcat folders
-    - step 4: copy the latest war files to tomcat folders
-    - step 5: start tomcat''')
+- step 1: stop tomcat
+- step 2: unzip devl.wrksp.ws.tar.gz and update wrksp.ws.war WEB-INF
+- step 3: clean tomcat folders
+- step 4: copy the latest war files to tomcat folders
+- step 5: start tomcat''')
     definition {
         cpsScm {
             scm {
@@ -56,7 +52,7 @@ pipelineJob('banner_page_build/deploy_dev') {
 }
 
 pipelineJob('banner_page_build/deploy_test') {
-    description("")
+  
     definition {
         cpsScm {
             scm {
