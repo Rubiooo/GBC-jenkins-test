2633e9492fb8015c5b7816d73926a6da8b00fa67
Jimmy Chen <maplerubiooo@gmail.com>
2018-08-24 12:40:39 -0400

    add new job and defintion

---
+++ pipelines/scheduled/auto_devl.groovy
@@ -0,0 +1,17 @@
+
+folder('scheduled_job')
+pipelineJob('scheduled_job/devl_nightly') {
+    description('''load script to devl and deploy war to devl every morning at 2am
+''')
+    triggers {
+        cron('0 2 * * *')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git('ssh://git@gitrepo.georgebrown.ca:7999/dev/jenkins-dsl.git', 'master')
+            }
+            scriptPath("pipelines/scheduled/auto_devl.groovy")
+        }
+    }
+}
