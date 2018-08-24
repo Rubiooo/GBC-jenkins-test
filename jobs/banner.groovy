955f7a5732861fd4a8d5a412a3e9a2f301700a74
Jimmy Chen <maplerubiooo@gmail.com>
2018-08-24 12:47:31 -0400

    remove crone job from gbc_build

--- jobs/banner.groovy
+++ jobs/banner.groovy
@@ -12,12 +12,7 @@ pipelineJob('banner_page_build/build_baseline') {
 }
 
 pipelineJob('banner_page_build/build_gbc') {
-  description('''Build the war files and upload to artifactory.
-scheduled to run everyday afternoon at 4:30pm.
-''')
-    triggers {
-        corn('30 16 * * *')
-    }
+  description('''Build the war files and upload to artifactory.''')    
     definition {
         cpsScm {
             scm {
