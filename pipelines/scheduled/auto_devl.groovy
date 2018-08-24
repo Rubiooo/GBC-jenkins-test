c6961d0a6c793056219216be1ee882914aee4366
Jimmy Chen <maplerubiooo@gmail.com>
2018-08-24 13:48:29 -0400

    change dryrun to false

--- pipelines/scheduled/auto_devl.groovy
+++ pipelines/scheduled/auto_devl.groovy
@@ -9,15 +9,15 @@ node {
               parameters: [string(name: 'Environment', value: 'devl'),
                   string(name: 'Username', value: 'jenkins'),
                   [$class: 'com.michelin.cio.hudson.plugins.passwordparam.PasswordParameterValue', description: 'db password', name: 'Password', value: 'abc123'],
-                  booleanParam(name: 'DryRun', value: true)]
+                  booleanParam(name: 'DryRun', value: false)]
       }
 
       stage("build and deploy devl") {
-          build job: 'banner_page_build/build_gbc'
+          build job: '/banner_page_build/build_gbc'
 
           build job: '/banner_page_build/deploy_dev'
       }
-      
+
       slackSend (channel: 'db-promotion', message: "====== auto devl Finish ======")
   } //timestamps
 } //node
