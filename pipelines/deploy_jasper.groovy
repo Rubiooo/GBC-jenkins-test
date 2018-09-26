9bad16882d237299aa0bc8ff90b8a5aaa330e1d0
Jimmy Chen <maplerubiooo@gmail.com>
2018-09-26 13:42:58 -0400

    deploy_jasper.groovy

--- pipelines/deploy_jasper.groovy
+++ pipelines/deploy_jasper.groovy
@@ -2,21 +2,21 @@ node {
 
   def environment="devl\ntest\nprod\nprds"
   def serverList = ['devl':'DEVL', 'test':'TEST', 'prod':'PROD', 'prds':'PRDS']
-  def sourceList = ['devl':'devl', 'test':'devl', 'prds':'test', 'prod':'prds']
-  def jasperList = ['devl':'devl.host.local',
-  'test':'test.host.local',
-  'prod':'tbd',
-  'prds':'tbd']
-  def tomcatList = ['devl':'/path/to/tomcat/bin',
-  'test':'/path/to/tomcat/bin',
-  'prod':'tbd',
-  'prds':'tbd']
-  def rootFolders = ['devl':'/path/to/jasperserver/buildomatic',
-  'test':'/path/to/jasperserver/buildomatic',
-  'prod':'tbd',
-  'prds':'tbd']
+  def promotionPath ="devl->devl\ndevl->test\ntest->prds\nprds->prod"
+  def jasperList = ['devl':'ban9appnav01d.gbcdev.local',
+  'test':'ban9pb01u.gbcuat.local',
+  'prod':'ban9pb01u.gbcuat.local',
+  'prds':'ban9pb01u.gbcuat.local']
+  def tomcatList = ['devl':'/u01/app/tomcat/instances/tomcat1/bin',
+  'test':'/opt/tomcat/instances/tomcat1/bin',
+  'prod':'/opt/tomcat/instances/tomcat1/bin',
+  'prds':'/opt/tomcat/instances/tomcat1/bin']
+  def rootFolders = ['devl':'/home/sysadmin/jasperreports-server-6.1.1-bin/buildomatic',
+  'test':'/home/sysadmin/jasperreports-server-6.1.1-bin/buildomatic',
+  'prod':'/home/sysadmin/jasperreports-server-6.1.1-bin/buildomatic',
+  'prds':'/home/sysadmin/jasperreports-server-6.1.1-bin/buildomatic']
 
-  def jrxmlPath = './gbc-*-reports/src/main/java/net/hedtech/gbc/*/reports'
+  def jrxmlPath = './gbc-*-reports/src/main/java/net/hedtech/banner/gbc/*/reports'
 
   def userInput
   def targetEnv
@@ -40,11 +40,20 @@ node {
   def reportList = []
 
   timestamps {
-    stage("Select environment and Jasper repor") {
+    stage("Choose env") {
+      userInput = input(
+       id: 'userInput', message: 'choose source and target environment', parameters: [
+       choice(choices: promotionPath, description: '', name: 'Promotion Path')
+       ])
+      sourceEnv = userInput.split("->")[0]
+      targetEnv = userInput.split("->")[1]
+    }
+
+    stage("Choose Jasper reports") {
       scm=checkout([
         $class: 'GitSCM',
         branches: [[name: "master"]],
-        userRemoteConfigs: [[url: "ssh://git@servername:7888/gbc/reponame.git"]]
+        userRemoteConfigs: [[url: "ssh://git@gitrepo.georgebrown.ca:7999/gbc/banner_pages_gbc.git"]]
         ])
 
       sh "rm -f fileList"
@@ -69,14 +78,6 @@ node {
         reportList.add(booleanParam(defaultValue: false, description: '', name: reportName))
       }
 
-      userInput = input(
-       id: 'userInput', message: 'Choose target environment', parameters: [
-        choice(choices: environment, description: '', name: 'env')
-        ])
-
-      targetEnv = userInput
-      sourceEnv = sourceList[targetEnv]
-
       targetHost = jasperList[targetEnv]
       sourceHost = jasperList[sourceEnv]
       targetServer = serverList[targetEnv]
@@ -85,7 +86,7 @@ node {
       sourceTomcat = tomcatList[sourceEnv]
       targetRootFolder = rootFolders[targetEnv]
       sourceRootFolder = rootFolders[sourceEnv]
-      sourceReportPath = "${sourceRootFolder}/resources/organReports"
+      sourceReportPath = "${sourceRootFolder}/resources/organizations/ellucian/Reports"
 
       userInput= input (message: 'Choose Jasper reports', parameters: reportList)
       wrap([$class: 'BuildUser']) {
@@ -95,20 +96,17 @@ node {
 
     stage("Stop Tomcat") {
       sh "ssh $sourceHost \"sudo -u tomcat ${sourceTomcat}/shutdown.sh\""
-      wrap([$class: 'BuildUser']) {
-        slackSend (channel: 'jenkins', message: ":black_square_button: " + " Start export Jasper report from " + sourceServer + ' - ' + sourceHost)
-      }
     }
     try {
           stage("Export files and Update Jasper reports") {
             sh "ssh $sourceHost \"sudo rm -rf ${sourceRootFolder}/gbc-jasper-reports.zip\""
             sh "ssh $sourceHost \"sudo rm -rf ${sourceRootFolder}/resources\""
 
-            sh "ssh $sourceHost \"cd $sourceRootFolder && sudo ./js-export.sh --uris /organizations/Reports --output-zip gbc-jasper-reports.zip\""
+            sh "ssh $sourceHost \"cd $sourceRootFolder && sudo ./js-export.sh --uris /organizations/ellucian/Reports --output-zip gbc-jasper-reports.zip\""
             sh "ssh $sourceHost \"cd $sourceRootFolder && unzip -o gbc-jasper-reports.zip\""
             sh "ssh $sourceHost \"cd $sourceRootFolder && chmod -R 755 resources\""
 
-            sh "scp $sourceHost:${sourceRootFolder}/resources/organizations/Reports/.folder.xml ."
+            sh "scp $sourceHost:${sourceRootFolder}/resources/organizations/ellucian/Reports/.folder.xml ."
             sh "sed -i '/<\\/folder>/d' .folder.xml"
             println "old folder.xml is below: "
             sh "cat .folder.xml"
@@ -134,7 +132,7 @@ node {
                     print "dataFolder: $dataFolder"
                     sh "cp ${jasperFile} main_jrxml.data"
                     sh "scp main_jrxml.data $sourceHost:${sourceReportPath}/${folderName}/${dataFolder}"
-                    sh "ssh $sourceHost \"cd $sourceRootFolder && sudo zip gbc-jasper-reports.zip resources/organizations/Reports/${folderName}/${dataFolder}/main_jrxml.data\""
+                    sh "ssh $sourceHost \"cd $sourceRootFolder && sudo zip gbc-jasper-reports.zip resources/organizations/ellucian/Reports/${folderName}/${dataFolder}/main_jrxml.data\""
                     sh "rm -f main_jrxml.data"
                   } else {
                     folder = folderName.toUpperCase()
@@ -144,7 +142,7 @@ node {
 
                     sh "cp ${jasperFile} ${subFile}"
                     sh "scp ${ROOTFOLDER}/${subFile} $sourceHost:${sourceReportPath}/${folderName}/${dataFolder}"
-                    sh "ssh $sourceHost \"cd $sourceRootFolder && sudo zip gbc-jasper-reports.zip resources/organizations/Reports/${folderName}/${dataFolder}/${subFile}\""
+                    sh "ssh $sourceHost \"cd $sourceRootFolder && sudo zip gbc-jasper-reports.zip resources/organizations/ellucian/Reports/${folderName}/${dataFolder}/${subFile}\""
                     sh "rm -f ${subFile}"
                   }
                 }
@@ -154,13 +152,10 @@ node {
             sh "grep -q -F 'EXAMPLE-NONEXIST' .folder.xml || echo '$LINE2' >> .folder.xml"
             println "new folder.xml is below: "
             sh "cat .folder.xml"
-            sh "scp .folder.xml $sourceHost:${sourceRootFolder}/resources/organizations/Reports"
-            sh "ssh $sourceHost \"cd $sourceRootFolder && sudo zip gbc-jasper-reports.zip resources/organizations/Reports/.folder.xml\""
+            sh "scp .folder.xml $sourceHost:${sourceRootFolder}/resources/organizations/ellucian/Reports"
+            sh "ssh $sourceHost \"cd $sourceRootFolder && sudo zip gbc-jasper-reports.zip resources/organizations/ellucian/Reports/.folder.xml\""
             sh "rm -rf .folder.xml"
 
-            wrap([$class: 'BuildUser']) {
-              slackSend (channel: 'jenkins', message: ":black_square_button: "+ " Start import Jasper report to " + targetServer + ' - ' + targetHost)
-            }
             if (targetEnv == 'devl') {
               sh "ssh $targetHost \"cd $targetRootFolder && sudo ./js-import.sh --input-zip gbc-jasper-reports.zip --update\""
               slackSend (channel: 'jenkins', message: ":ballot_box_with_check: Successfully deploy Jasper report from " + sourceServer + ' to ' + targetServer+"  "+BUILD_URL)
