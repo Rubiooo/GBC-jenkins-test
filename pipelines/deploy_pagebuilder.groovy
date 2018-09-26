57b2751c804b910ed4963002fefba56d9ce1a277
Jimmy Chen <maplerubiooo@gmail.com>
2018-09-26 13:36:13 -0400

    pagebuilder -test deployment

--- pipelines/deploy_pagebuilder.groovy
+++ pipelines/deploy_pagebuilder.groovy
@@ -1,116 +1,76 @@
-node {
+def virtFolder
+def cssFolder
+def tomcatPath
 
-  def environment="devl\ntest\nprod\nprds"
-  def targetHosts = ['devl':'gbcdev.local',
-  'test':'gbcdev.local',
-  'prod':'gbcdev.local',
-  'prds':'gbcdev.local']
-  def targetServers=['devl':'DEVL',
-  'test':'TEST',
-  'prod':'PROD',
-  'prds':'PRDS']
+timestamps {
+  stage("Choose env") {
+    userInput = input(
+     id: 'userInput', message: 'choose target environment', parameters: [
+     choice(choices: environment, description: '', name: 'target environment')
+     ])
+    targetEnv = userInput
+    targetServer = targetServers[targetEnv]
+    targetHost = targetHosts[targetEnv]
+    pagePath = pagePaths[targetEnv]
+    pageFolder = folderNames[targetEnv].split("/")[0]
+    virtFolder = folderNames[targetEnv].split("/")[1]
+    cssFolder = folderNames[targetEnv].split("/")[2]
+    tomcatPath =tomcatPaths[targetEnv]
+  }
 
-  def fileList = []
-  def targetServer
-  def pageName
-  def jasonFile
-  def prefix = ''
-  def nameList = []
-  def pages = []
-  def pageList = []
 
-  timestamps {
-    stage("Choose env") {
-      userInput = input(
-       id: 'userInput', message: 'choose target environment', parameters: [
-       choice(choices: environment, description: '', name: 'target environment')
-       ])
-
-       targetEnv = userInput
-       targetServer = targetServers[targetEnv]
-       targetHost = targetHosts[targetEnv]
-       pagePath = pagePaths[targetEnv]
-       tomcatPath =tomcatPaths[targetEnv]
-    }
-
-    stage("Stop Tomcat") {
-          sh "ssh $targetHost \"sudo -u tomcat ${tomcatPath}/shutdown.sh\""
-        }
-
-    try {
-        stage("Choose Pages") {
-          wrap([$class: 'BuildUser']) {
-          slackSend (channel: 'jenkins', message: ":black_square_button: "+  env.BUILD_USER +" start deploy PageBuilder in "+ targetServer +" - "+targetHost)
-        }
-          scm=checkout([
-          $class: 'GitSCM',
-          branches: [[name: "master"]],
-          userRemoteConfigs: [[url: "ssh://gitrepo/banner_extensibility.git"]]
-          ])
-
-          sh "rm -f fileList"
-          sh "find . -type f -name '*.json' > listJson;"
-          def list = readFile( "listJson" ).split( "\\r?\\n" )
-          sh "rm -f listJson"
-          for (file in list) {
-            def newline = file.toString()
-            fileList.add(newline)
-          }
-          HashSet<String> set = new HashSet<>();
-          for (line in fileList) {
-            pageName = line.trim().substring(line.lastIndexOf("/", line.lastIndexOf("/") - 1 ) + 1, line.lastIndexOf("/"))
-            if (!set.contains(pageName)) {
-              nameList.add(pageName)
-              set.add(pageName)
-            }
-          }
-          pages = nameList.sort()
-          for (page in pages) {
-            pageList.add(booleanParam(defaultValue: false, description: '', name: page))
-          }
-          userInput= input (message: 'Choose Pages', parameters: pageList)
-        }
-
-        stage("Update Pages") {
-          for (line in fileList) {
-            pageName = line.trim().substring(line.lastIndexOf("/", line.lastIndexOf("/") - 1 ) + 1, line.lastIndexOf("/"))
-
-            if (userInput[pageName]) {
-              println pageName
-              jsonFile = line.trim().substring(line.lastIndexOf("/") + 1)
-              println "jsonFile: $jsonFile"
-              prefix = jsonFile.trim().substring(0, fileName.indexOf("."))
-              def fileName = line.trim().substring(line.lastIndexOf("/") + 1)
-              println "jsonFile: $jsonFile"
-              prefix = jsonFile.trim().substring(0, jsonFile.indexOf("."))
-              println "prefix: $prefix"
-              def fileName = jsonFile.trim().substring(jsonFile.indexOf(".") + 1)
-              println "fileName: $fileName"
-              sh "cp $line $fileName"
-              switch (prefix) {
-                case "pages":
-                  sh "scp $fileName $targetHost:${pagePath}/pbTemp.page/"
-                  break
-                case "virtualDomains":
-                  sh "scp $fileName $targetHost:${pagePath}/pbTemp.virtualDomain/"
-                  break
-                case "css":
-                    sh "scp $fileName $targetHost:${pagePath}/pbTemp.css/"
-                    break
-                default:
-                  slackMessage += "unknow category "+ prefix + "\n"
-                  throw "unknow category"
-              }
-              sh "rm -rf $fileName"
-            }
-          }
-          slackSend (channel: 'jenkins', message: ":ballot_box_with_check: Successfully deploy PageBuilder in " + targetServer +" "+BUILD_URL)
-      }
-    } catch (error) {
-      slackSend (channel: 'jenkins', message: ":warning: deploy PageBuilder failed, please check logs -> ${BUILD_URL}")
-    }
-    stage("Start Tomcat") {
-      sh "ssh $targetHost \"sudo -u tomcat ${tomcatPath}/startup.sh\""
+  stage("Stop Web Application") {
+    if ('devl'.equals(targetEnv)) {
+      sh "ssh $targetHost \"sudo -u tomcat ${tomcatPath}/shutdown.sh\""
+    } else {
+      sh "ssh $targetHost \"java -cp /u01/app/oracle/Middleware/wlserver/server/lib/weblogic.jar weblogic.Deployer -adminurl t3://gbcxe01u:7001 -username weblogic -password password1 -name BannerExtensibility -undeploy -timeout 300\""
     }
   }
+
+  try {
+    stage("Choose Pages") {
+      wrap([$class: 'BuildUser']) {
+        slackSend (channel: 'jenkins', message: ":black_square_button: "+  env.BUILD_USER +" start deploy PageBuilder in "+ targetServer +" - "+targetHost)
+      }
+      scm=checkout([
+      $class: 'GitSCM',
+      branches: [[name: "master"]],
+      userRemoteConfigs: [[url: "ssh://git@gitrepo.georgebrown.ca:7999/gbc/banner_extensibility.git"]]
+      ])
+
+      sh "rm -f fileList"
+      sh "find . -type f -name '*.json' > listJson;"
+      def list = readFile( "listJson" ).split( "\\r?\\n" )
+      sh "rm -f listJson"
+      for (file in list) {
+        def newline = file.toString()
+              sh "scp $fileName $targetHost:${pagePath}/${virtFolder}/"
+              break
+            case "css":
+              sh "scp $fileName $targetHost:${pagePath}/${cssFolder}/"
+              break
+            default:
+              throw "unknow file category"
+          }
+          sh "rm -rf $fileName"
+        }
+      }
+      if (!'devl'.equals(targetEnv)) {
+        sh "ssh $targetHost \"chown -R banner:banner ${pagePath}/*\""
+      }
+      slackSend (channel: 'jenkins', message: ":ballot_box_with_check: Successfully deploy PageBuilder in " + targetServer +" "+BUILD_URL)
+    }
+  } catch(error) {
+    currentBuild.result = 'FAILURE'
+    slackSend (channel: 'jenkins', message: ":warning: deploy PageBuilder failed, please check logs -> ${BUILD_URL}")
+  }
+
+  stage("Start Web Application") {
+    if ('devl'.equals(targetEnv)) {
+      sh "ssh $targetHost \"sudo -u tomcat ${tomcatPath}/startup.sh\""
+    } else {
+      sh "ssh $targetHost \"java -cp /u01/app/oracle/Middleware/wlserver/server/lib/weblogic.jar weblogic.Deployer -adminurl t3://gbcxe01u:7001 -username weblogic -password password1 -targets Banner9-App -deploy /u02/banapp/staging/ban9app/banner_extensibility_9.2/current/dist/BannerExtensibility.war -timeout 300\""
+    }
+  }
+}
 }
